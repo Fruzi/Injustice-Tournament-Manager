4d32e3d59dbe8de63b5c49f013301218487b3c01
fruzi <uzifr@post.bgu.ac.il>
2017-11-17 20:53:27 +0200

    started implementing roster
    still having issues with loading missing assets

---
+++ characterRow.js
@@ -0,0 +1,16 @@
+function characterRow(length, inital_x, inital_y){
+   this.length=length;
+   this.characterButtons=new Array(length);
+   this.inital_x=inital_x;
+   this.inital_y=inital_y;
+   for (i=0; i<length ; i++){
+     var x_value=inital_x+35*i;
+     this.characterButtons[i]=new characterButton(x_value,inital_y,i);
+   }
+}
+
+characterRow.prototype.show = function(){
+  for (i=0; i<this.length ; i++){
+    this.characterButtons[i].show();
+  }
+}
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+var MAX_CHARACTER_THUMBART = 19
 var level = -1;
 var bgArt = new Array(2);
-var characterArt1 = new Array(50);
-var characterArt2 = new Array(50);
+var characterArt = new Array(MAX_CHARACTER_THUMBART*2);
+var characterRow1;
+var characterRow2;
 var canvas;
 var resetButton;
 var modeButtons = new Array(3);
@@ -10,15 +12,24 @@ function preload() {
   for (var i = 0; i < bgArt.length; i++) {
     bgArt[i] = loadImage('assets/bg/bg' + i + '.jpg');
   }
-  // for (var i = 0; i < 50; i++) {
-  //   characterArt[i] = loadImage('assets/characters/character' + i + '.jpg');
-  // }
+  characterRow1 = new characterRow(MAX_CHARACTER_THUMBART, 60, 0)
+  characterRow2 = new characterRow(MAX_CHARACTER_THUMBART ,60 ,60)
+  var path;
+  for (var i = 0; i < 15; i++) {
+    if(i<=18){
+      path = 'assets/characters/character' + '_a' + i + '.jpg';
+    }
+    else{
+        path = 'assets/characters/character' + '_b' + i-19 + '.jpg';
+    }
+     characterArt[i] = loadImage(path,console.log("success image " +i),console.log("fail image " + i)/*loadImage('assets/characters/locked_character.js')*/);
+  }
 }
 
 function setup() {
   canvas = createCanvas(1280 * 0.7, 720 * 0.7);
   imageMode(CORNER);
-  createButtons();
+  createModeButtons();
 }
 
 function draw() {
@@ -27,12 +38,13 @@ function draw() {
     displayModeButtons();
   } else if (level < 100) { //game modes
     background(bgArt[1]);
+    displayCharacterThumbs();
   } else { //fight
     background(bgArt[2]);
   }
 }
 
-function createButtons() {
+function createModeButtons() {
   resetButton = createButton('RESET');
   resetButton.mousePressed(reset);
   resetButton.position(10, height - 15);
@@ -51,9 +63,15 @@ function mousePressed() {
   for (var i = 0; i < modeButtons.length; i++) {
     if (modeButtons[i].contains(mouseX, mouseY)) {
       level = modeButtons[i].mode;
+      createImage
       break;
     }
   }
+}
+
+function displayCharacterThumbs() {
+    characterRow1.show();
+    characterRow2.show()
 }
 
 function reset() {
