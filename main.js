81f56e752fa66d8bedc4489f1d51aa9d4eeb0eb1
Asaf Mizrachi <asafmiz401@gmail.com>
2017-11-18 01:33:09 +0200

    rules system added
    Replace all whitespaces with tabs.

--- main.js
+++ main.js
@@ -5,30 +5,39 @@ var characterArt2 = new Array(50);
 var canvas;
 var resetButton;
 var modeButtons = new Array(3);
+var submitButton;
+var rules = new Array();
+var eventsCounter = 0;
+var submitted = false;
 
 function preload() {
   for (var i = 0; i < bgArt.length; i++) {
     bgArt[i] = loadImage('assets/bg/bg' + i + '.jpg');
   }
   // for (var i = 0; i < 50; i++) {
-  //   characterArt[i] = loadImage('assets/characters/character' + i + '.jpg');
+  //   characterArt1[i] = loadImage('assets/characters/character' + i + '.jpg');
   // }
+  //TODO load buttons art
 }
 
 function setup() {
   canvas = createCanvas(1280 * 0.7, 720 * 0.7);
   imageMode(CORNER);
   createButtons();
+  createRules();
 }
 
 function draw() {
-  if (level == -1) { // select mode
+  if (level == 100) { //fight mode
+    console.log("fightmode");
+    level = -1;
+  } else if (level == -1) { // select mode
     background(bgArt[0]);
     displayModeButtons();
   } else if (level < 100) { //game modes
     background(bgArt[1]);
-  } else { //fight
-    background(bgArt[2]);
+    submitButton.show();
+    updateRules();
   }
 }
 
@@ -39,6 +48,12 @@ function createButtons() {
   modeButtons[0] = new modeButton(600, 100, 0, "3v3 Draft");
   modeButtons[1] = new modeButton(600, 200, 1, "5v5 Draft");
   modeButtons[2] = new modeButton(600, 300, 2, "3v3 All Random");
+  submitButton = new SubmitButton(width / 2 - 50, height - 100, "Submit");
+}
+
+function createRules() {
+  rules.push("player 1 chooses:");
+  rules.push("player 2 bans:");
 }
 
 function displayModeButtons() {
@@ -48,14 +63,47 @@ function displayModeButtons() {
 }
 
 function mousePressed() {
-  for (var i = 0; i < modeButtons.length; i++) {
-    if (modeButtons[i].contains(mouseX, mouseY)) {
-      level = modeButtons[i].mode;
-      break;
+  console.log("ping");
+  if (level >= 0) {
+    if (submitButton.contains(mouseX, mouseY)) {
+      submitted = true;
+    }
+  } else {
+    for (var i = 0; i < modeButtons.length; i++) {
+      if (modeButtons[i].contains(mouseX, mouseY)) {
+        level = modeButtons[i].mode;
+        break;
+      }
     }
   }
 }
 
 function reset() {
   level = -1;
+  rules = [];
+  eventsCounter = 0;
+}
+
+function updateRules() {
+  if (submitted) {
+    submitted = false;
+    eventsCounter++;
+  }
+  if (level == 100) { //fight mode
+    push();
+    fill(255);
+    textSize(20);
+    text('fight', 50, 50);
+    pop();
+  } else if (level == 0) { //3v3 draft
+    push();
+    fill(255);
+    textSize(20);
+    text(rules[eventsCounter], 50, 50);
+    pop();
+    if (eventsCounter == rules.length) {
+      level = 100;
+      eventsCounter = 0;
+    }
+  }
 }
